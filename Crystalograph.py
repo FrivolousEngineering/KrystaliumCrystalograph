2b1a83b6cd49f5a7cce2ba112011734c977bfb73
Jaime van Kessel <nallath@gmail.com>
2023-12-09 18:29:10 +0100

    Codestyle cleanups

--- Crystalograph.py
+++ Crystalograph.py
@@ -1,25 +1,21 @@
-from typing import Tuple
+from typing import Tuple, Optional
 
 import numpy
 import numpy as np
 import cv2
 import math
 from scipy.signal import savgol_filter
-import random
+
 # Typing helpers
 Image = np.ndarray
 Point = Tuple[int, int]
 Color = Tuple[int, int, int]
 
-
-
-
 NUM_SEGMENTS_PER_LENGTH = 0.5
 
 
 class Crystalograph:
-
-    def __init__(self):
+    def __init__(self) -> None:
         self._colors = {}
         self._colors["white"] = (255, 255, 255)
         self._colors["black"] = (0, 0, 0)
@@ -56,10 +52,10 @@ class Crystalograph:
         cv2.line(self._image, (int(width / 2), 0), (int(width / 2), height), line_color, 1)
         cv2.line(self._image, (0, int(height / 2)), (width, int(height / 2)), line_color, 1)
 
-    def generateCirclePolyLines(self, center: Point, radius: int, begin_angle: int = 0, end_angle: int = 90, *, noise: float = 0.1, smooth_noise: bool = True):
+    def generateCirclePolyLines(self, center: Point, radius: int, begin_angle: int = 0, end_angle: int = 90, *,
+                                noise: float = 0.1, smooth_noise: bool = True):
         circle_length = (2 * math.pi * radius) * ((end_angle - begin_angle) / 360)
         num_segments = int(circle_length * NUM_SEGMENTS_PER_LENGTH)
-
 
         begin_angle_rad = math.radians(begin_angle + 180)
         end_angle_rad = math.radians(end_angle + 180)
@@ -70,11 +66,13 @@ class Crystalograph:
 
         for segment in range(num_segments):
             # Calculate where the circle should be.
-            circle = (-math.sin(segment * spacing_between_angle + begin_angle_rad) * radius, math.cos(segment * spacing_between_angle + begin_angle_rad) * radius)
+            circle = (-math.sin(segment * spacing_between_angle + begin_angle_rad) * radius,
+                      math.cos(segment * spacing_between_angle + begin_angle_rad) * radius)
             pts.append(circle)
 
         pts = np.array(pts, np.int32)
-        noise_multiplier = self.generateNoiseMultiplierForCircle(num_segments, noise, smooth_noise, int(num_segments / 8))
+        noise_multiplier = self.generateNoiseMultiplierForCircle(num_segments, noise, smooth_noise,
+                                                                 int(num_segments / 8))
         pts = numpy.multiply(pts, noise_multiplier)
 
         # Now ensure that the centre of our curve is set correctly!
@@ -85,8 +83,9 @@ class Crystalograph:
         pts = np.array(pts, np.int32)
         return pts
 
-
-    def generateNoiseMultiplierForCircle(self, num_segments: int, noise: float, smooth_noise: bool, num_cap_segments_limit_noise: int = 0) -> np.array:
+    @staticmethod
+    def generateNoiseMultiplierForCircle(num_segments: int, noise: float, smooth_noise: bool,
+                                         num_cap_segments_limit_noise: int = 0) -> np.array:
         """
 
         :param num_segments:
@@ -126,8 +125,11 @@ class Crystalograph:
 
         return noise_multiplier
 
-    def drawCircleWithPolyLines(self, color: Color, center: Point, radius: int, begin_angle: int = 0, end_angle: int = 90, line_thickness: int = 2, *, noise: float = 0.1, smooth_noise: bool = True, is_closed: bool = False) -> Image:
-        pts = self.generateCirclePolyLines(center, radius, begin_angle, end_angle, noise=noise, smooth_noise=smooth_noise)
+    def drawCircleWithPolyLines(self, color: Color, center: Point, radius: int, begin_angle: int = 0,
+                                end_angle: int = 90, line_thickness: int = 2, *, noise: float = 0.1,
+                                smooth_noise: bool = True, is_closed: bool = False) -> Image:
+        pts = self.generateCirclePolyLines(center, radius, begin_angle, end_angle, noise=noise,
+                                           smooth_noise=smooth_noise)
         pts = pts.reshape((-1, 1, 2))
 
         # Actually draw them
@@ -142,52 +144,56 @@ class Crystalograph:
             cv2.blur(self._image, ksize=(blur_ksize, blur_ksize), dst=self._image)
 
     # Debug function for convenience
-    def drawLines(self, line_thickness = 2, override_color = None):
-
-
+    def drawLines(self, line_thickness: int = 2, override_color: Optional[Color] = None):
         if override_color:
             self.drawCircleWithPolyLines(override_color, self._center, 100, 270, 360, line_thickness=line_thickness)
         else:
-            self.drawCircleWithPolyLines(self.getColor("RED"), self._center, 100, 270, 360, line_thickness=line_thickness)
+            self.drawCircleWithPolyLines(self.getColor("RED"), self._center, 100, 270, 360,
+                                         line_thickness=line_thickness)
 
         if not override_color:
             override_color = self.getColor("BLUE")
 
         self.drawCircleWithPolyLines(override_color, self._center, 100, 0, 90,
-                                        line_thickness=line_thickness)
+                                     line_thickness=line_thickness)
         self.drawCircleWithPolyLines(override_color, self._center, radius=150, begin_angle=30,
-                                        end_angle=45,
-                                        line_thickness=line_thickness)
+                                     end_angle=45,
+                                     line_thickness=line_thickness)
         self.drawCircleWithPolyLines(override_color, self._center, 150, 60, 75,
-                                        line_thickness=line_thickness)
+                                     line_thickness=line_thickness)
 
-    def drawCircles(self, line_thickness = 2, override_color = None):
-        small_cirlce_radius = 50
+    def drawCircles(self, line_thickness: int = 2, override_color: Optional[Color] = None) -> None:
+        small_circle_radius = 50
         large_circle_radius = 100
 
-        normalized_small_circle_radius = 0.05 * (100/small_cirlce_radius)
+        normalized_small_circle_radius = 0.05 * (100 / small_circle_radius)
         normalized_large_circle_radius = 0.05 * (100 / large_circle_radius)
 
         if not override_color:
             override_color = self.getColor("GREEN")
 
-        self.drawCircleWithPolyLines(override_color, tuple(numpy.add(self._center, (large_circle_radius, large_circle_radius))),
-                                        large_circle_radius, 0, 360, is_closed=True, noise=normalized_large_circle_radius,
-                                        line_thickness=line_thickness)
         self.drawCircleWithPolyLines(override_color,
-                                        tuple(numpy.add(self._center, (small_cirlce_radius + 14, small_cirlce_radius + 14))),
-                                        small_cirlce_radius, 0, 360, is_closed=True, noise=normalized_small_circle_radius,
-                                        line_thickness=line_thickness)
+                                     tuple(numpy.add(self._center, (large_circle_radius, large_circle_radius))),
+                                     large_circle_radius, 0, 360, is_closed=True, noise=normalized_large_circle_radius,
+                                     line_thickness=line_thickness)
+        self.drawCircleWithPolyLines(override_color,
+                                     tuple(
+                                         numpy.add(self._center, (small_circle_radius + 14, small_circle_radius + 14))),
+                                     small_circle_radius, 0, 360, is_closed=True, noise=normalized_small_circle_radius,
+                                     line_thickness=line_thickness)
 
-    def drawDoubleCircle(self, thickness = 6, radius = 150, start_angle = 185, end_angle = 265):
+    def drawDoubleCircle(self, thickness: int = 6, radius: int = 150, start_angle: int = 185,
+                         end_angle: int = 265) -> None:
         # As the noise is based on the radius, we want to normalize it in this case
-        normalized_noise = 0.08 * (100/radius)
+        normalized_noise = 0.08 * (100 / radius)
 
-        pts_top = self.generateCirclePolyLines(self._center, int(radius - thickness / 2), start_angle, end_angle, noise=normalized_noise,
-                                      smooth_noise=True)
+        pts_top = self.generateCirclePolyLines(self._center, int(radius - thickness / 2), start_angle, end_angle,
+                                               noise=normalized_noise,
+                                               smooth_noise=True)
 
-        pts_bottom = self.generateCirclePolyLines(self._center, int(radius + thickness / 2), start_angle, end_angle, noise=normalized_noise,
-                                      smooth_noise=True)
+        pts_bottom = self.generateCirclePolyLines(self._center, int(radius + thickness / 2), start_angle, end_angle,
+                                                  noise=normalized_noise,
+                                                  smooth_noise=True)
 
         # Due to winding order, we need to flip the bottom points again
         pts_bottom = numpy.flipud(pts_bottom)
@@ -197,8 +203,8 @@ class Crystalograph:
         # Actually draw them
         cv2.fillPoly(self._image, [pts], self.getColor("YELLOW"))
 
-    def draw(self, line_thickness = 2, override_color = None):
-        self.drawLines(line_thickness, override_color = override_color)
+    def draw(self, line_thickness: int = 2, override_color: Optional[Color] = None) -> None:
+        self.drawLines(line_thickness, override_color=override_color)
         self.drawCircles(line_thickness=line_thickness, override_color=override_color)
         self.drawDoubleCircle(thickness=1, radius=150, start_angle=185, end_angle=209)
 
@@ -207,10 +213,9 @@ class Crystalograph:
 
         self.drawDoubleCircle(thickness=1, radius=130, start_angle=197, end_angle=221)
 
-    def drawVisualTest(self):
-
+    def drawVisualTest(self) -> Image:
         # I know it's hella hacky. But suuuusssssh
-        #applyFlickerToColors()
+        # applyFlickerToColors()
 
         self.draw(line_thickness=4)
         self.applyBlooming(gausian_ksize=25, blur_ksize=25)
@@ -225,7 +230,6 @@ class Crystalograph:
         # applyBlooming(image, gausian_ksize=3, blur_ksize=0)
         self.drawTargetLines()
         return self._image
-
 
 
 '''def applyFlickerToColors():
@@ -265,10 +269,6 @@ class Crystalograph:
     BLUE = (int(blue_rgb[0][0][0] + 0.5), int(blue_rgb[0][0][1] + 0.5), int(blue_rgb[0][0][2] + 0.5))
     pass'''
 
-
-
-
-
 if __name__ == '__main__':
     crystalograph = Crystalograph()
     crystalograph.createEmptyImage((1024, 768))
@@ -276,4 +276,3 @@ if __name__ == '__main__':
     img = crystalograph.drawVisualTest()
     cv2.imshow('Test', cv2.cvtColor(img, cv2.COLOR_RGB2BGR))
     cv2.waitKey()
-
