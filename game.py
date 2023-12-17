cdfa17ba4c8725b6d59fe2fa99c147e3b8ab801a
Jaime van Kessel <nallath@gmail.com>
2023-12-17 17:39:55 +0100

    Add fadein

--- game.py
+++ game.py
@@ -1,5 +1,6 @@
 import contextlib
 import math
+import random
 from typing import Tuple
 
 # This suppresses the `Hello from pygame` message.
@@ -8,6 +9,41 @@ with contextlib.redirect_stdout(None):
 
 import numpy as np
 import Crystalograph
+
+
+class Fader:
+    def __init__(self):
+        self._fading = "in"
+        self._alpha = 255
+        sr = pygame.display.get_surface().get_rect()
+        self._veil = pygame.Surface(sr.size)
+        self._veil.fill((0, 0, 0))
+
+    def fadeOut(self):
+        self._fading = "out"
+
+    def fadeIn(self):
+        self._fading = "in"
+
+    def draw(self, screen):
+        if self._alpha > 0:
+            self._veil.set_alpha(self._alpha)
+            screen.blit(self._veil, (0, 0))
+
+    def update(self):
+        if self._fading == "in":
+            if self._alpha > 0:
+                self._alpha -= 1
+        elif self._fading == "out":
+            if self._alpha < 255:
+                self._alpha += 1
+
+        if self._alpha < 0:
+            self._fading = None
+            self._alpha = 0
+        if self._alpha > 255:
+            self._fading = None
+            self._alpha = 255
 
 
 if __name__ == '__main__':
@@ -39,20 +75,21 @@ if __name__ == '__main__':
 
     #Middle
     crystalograph.addLineToDraw(line_type="line", thickness=5, radius=175, begin_angle=0, end_angle=360,
-                                base_color="red", center=(center_x, center_y),
+                                base_color="blue", center=(center_x, center_y),
                                 spikes = [(60, 20, 0.2),
                                           (10, 5, 0.2),
                                           (300, 25, 0.2)])
 
     #Outer
     crystalograph.addLineToDraw(line_type="line", thickness=5, radius=275, begin_angle=0, end_angle=360,
-                                base_color="gree", center=(center_x, center_y),
+                                base_color="blue", center=(center_x, center_y),
                                 spikes=[(80, 25, 0.15),
                                         (120, 5, 0.2),
                                         (150, 5, 0.15),
                                         (270, 25, 0.2)])
 
     crystalograph.setup()
+    fader = Fader()
     while running:
         crystalograph.createEmptyImage((screen_width, screen_height))
         image = crystalograph.draw()
@@ -66,15 +103,20 @@ if __name__ == '__main__':
                     running = False
 
             if event.type == pygame.KEYDOWN and event.key == pygame.K_F11:
-                # Toggle fullscreen mode
                 pygame.display.toggle_fullscreen()
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_x:
+                fader.fadeIn()
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_c:
+                fader.fadeOut()
+
+        fader.update()
 
         # This is where we insert the numpy array.
         # Because pygame and numpy use different coordinate systems,
         # the numpy image has to be flipped and rotated, before being blit.
         img = pygame.surfarray.make_surface(np.fliplr(np.rot90(image, k=-1)))
         screen.blit(img, (0, 0))
-
+        fader.draw(screen)
         pygame.display.flip()
         crystalograph.update()
         clock.tick()
