f3fba6a88afe039dd444fab1ceec9f6c18114a2f
Jaime van Kessel <nallath@gmail.com>
2024-12-23 14:38:18 +0100

    Add command parsing to enable and disable memory reading

--- RFIDReader/RFIDReader.ino
+++ RFIDReader/RFIDReader.ino
@@ -5,19 +5,20 @@
 #define RST_PIN 0
 
 MFRC522 mfrc522(SS_PIN, RST_PIN); // Instance of the class
+MFRC522::MIFARE_Key key;
 
 int errorCount = 0;
 const int errorThreshold = 3;
 String detected_tag = "";
-MFRC522::MIFARE_Key key;  // Global key declaration
+bool printMemory = false;
 
 void setup() { 
   Serial.begin(9600);
-  SPI.begin();
-  mfrc522.PCD_Init();
+  SPI.begin(); // Init SPI bus
+  mfrc522.PCD_Init(); // Init MFRC522 
   Serial.println("Reader has booted");
 
-  // Initialize the key to 0xFFFFFFFFFFFF (default key)
+  // Initialize default key
   for (byte i = 0; i < 6; i++) {
     key.keyByte[i] = 0xFF;
   }
@@ -40,7 +41,6 @@ void printHex(byte *buffer, byte bufferSize) {
   Serial.println();
 }
 
-
 void readCardMemory() {
   byte buffer[18];
   byte size = sizeof(buffer);
@@ -55,7 +55,6 @@ void readCardMemory() {
       continue;
     }
 
-    // Read block
     status = mfrc522.MIFARE_Read(block, buffer, &size);
     if (status == MFRC522::STATUS_OK) {
       Serial.print("Block ");
@@ -71,24 +70,43 @@ void readCardMemory() {
   mfrc522.PCD_StopCrypto1();
 }
 
+void processCommand(String command) {
+  command.trim();
+  if (command == "MREAD ON") {
+    printMemory = true;
+    Serial.println("Memory print enabled");
+  } else if (command == "MREAD OFF") {
+    printMemory = false;
+    Serial.println("Memory print disabled");
+  } else {
+    Serial.println("Unknown command");
+  }
+}
 
 void loop() {
+  if (Serial.available() > 0) {
+    String command = Serial.readStringUntil('\n');
+    processCommand(command);
+  }
+
   if (mfrc522.PICC_IsNewCardPresent() && mfrc522.PICC_ReadCardSerial()) {
-    if (detected_tag == "") { // No card present
+    if (detected_tag == "") {
       detected_tag = toHexString(mfrc522.uid.uidByte, mfrc522.uid.size);
       Serial.print("Tag found: ");
       Serial.println(detected_tag);
-      readCardMemory(); // Read and print card memory
+      if (printMemory) {
+        readCardMemory();
+      }
     }
-    errorCount = 0; // Reset error counter when card is detected, even if already present
+    errorCount = 0;
   } else {
-    if (detected_tag != "") { // Card was previously there.
+    if (detected_tag != "") {
       errorCount++;
       if (errorCount > errorThreshold) {
         Serial.print("Tag lost: ");
         Serial.println(detected_tag);
         detected_tag = "";
-        errorCount = 0; // Reset error count after detecting removal
+        errorCount = 0;
         mfrc522.PICC_HaltA();
         mfrc522.PCD_StopCrypto1();
       }
